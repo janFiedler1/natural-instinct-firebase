184eae0d955c0e2e5dfb3d0ddaf9c7e2fdd97153
janFiedler1 <50682895+janFiedler1@users.noreply.github.com>
2024-10-23 16:15:27 -0400

    remove comment

--- src/components/PieceComponent.js
+++ src/components/PieceComponent.js
@@ -24,33 +24,3 @@ function Piece({piece}) {
 }
 
 export default Piece;
-
-
-
-// import React from "react";
-// import '../component-css/PieceComponent.css';
-
-// /**
-//  * @param {int} key - unique identifier
-//  * @param {string} title - name of the piece
-//  * @param {url} path - path to the image
-//  * @param {url} payment_link - payment link
-//  */
-// function Piece(prop) {
-//     return (
-//         <React.Fragment>
-//             <a href={prop.payment_link}>
-//                 <div key={prop.id} className={"piece-container"+(prop.status=="sold" ? " grayed-out" : "")}>
-//                     {/* <div className="piece-title">{prop.title}</div> */}
-//                     <img src={prop.path}/>
-//                     <div className="piece-info">
-//                         <span></span>
-//                         <span></span>
-//                     </div>
-//                 </div>
-//             </a>
-//         </React.Fragment>
-//     )
-// }
-
-// export default Piece;
